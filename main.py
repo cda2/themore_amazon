b4778c915f93900b4643d0d5a388c249d41cd48a
cda2 <qq23667@gmail.com>
2023-06-25 04:40:44 +0900

    fix: 6천원 이상 결제되지 않도록 기본으로 0.03 달러 덜 결제
    - 잡다 코드들 추가

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+import argparse
+import datetime
 import logging
+from argparse import ArgumentParser
+from functools import wraps
+from os import PathLike
 from pathlib import Path
 from typing import Optional, Mapping, Any, TypedDict
 
 import yaml
-from playwright.sync_api import sync_playwright, Browser, Page, Response, ElementHandle, TimeoutError, Playwright
+from playwright.sync_api import (
+    sync_playwright,
+    Browser,
+    Page,
+    Response,
+    ElementHandle,
+    TimeoutError,
+    Playwright,
+)
 
-SECOND_IN_MILLISECONDS: int = 1000
+SEC_IN_MIL: int = 1000
+GLOBAL_TIMEOUT: int = 5 * SEC_IN_MIL
 
 
 def init_logger() -> None:
@@ -25,11 +39,12 @@ def init_browser(playwright: Playwright, headless: bool = False) -> Browser:
     return browser
 
 
-def init_page(browser: Browser) -> Page:
+def init_page(browser: Browser, default_timeout: int = GLOBAL_TIMEOUT) -> Page:
     logging.info("initializing page...")
     page: Page = browser.new_page()
     # set viewport
     page.set_viewport_size({"width": 1920, "height": 1080})
+    page.set_default_timeout(GLOBAL_TIMEOUT)
     logging.info("page initialized.")
     return page
 
@@ -38,7 +53,11 @@ def goto_url(page: Page, url: str) -> Response:
     return page.goto(url)
 
 
-def get_5999_won_concurrency(page: Page, is_safe: bool = False) -> float:
+def now_str(dt_fmt: str = "%Y%m%d_%H%M%S") -> str:
+    return datetime.datetime.now().strftime(dt_fmt)
+
+
+def get_5999_won_concurrency(page: Page, is_safe: bool = True) -> float:
     url: str = "https://www.thecashback.kr/exchangerate.php"
     logging.info(f"getting 5999 won price from {url}...")
     goto_url(page, url)
@@ -46,7 +65,7 @@ def get_5999_won_concurrency(page: Page, is_safe: bool = False) -> float:
     price: str = left_input.input_value()
     logging.info(f"5999 won price: {price}")
 
-    return float(price) - 0.05 if is_safe else float(price)
+    return float(price) - 0.03 if is_safe else float(price)
 
 
 def goto_amazon(page: Page) -> None:
@@ -79,9 +98,13 @@ def login(page: Page, email: str, password: str) -> None:
 
 def captcha_solve(page: Page, password: str) -> None:
     try:
-        captcha_element: ElementHandle = page.wait_for_selector("#auth-captcha-image", timeout=10000)
+        captcha_element: ElementHandle = page.wait_for_selector(
+            "#auth-captcha-image",
+        )
         logging.info("found captcha element.")
-        password_element: ElementHandle = page.wait_for_selector("#ap_password", timeout=10000)
+        password_element: ElementHandle = page.wait_for_selector(
+            "#ap_password",
+        )
         logging.info("found password element. typing password...")
         password_element.type(password)
         logging.info("typed password. solving captcha...")
@@ -99,20 +122,41 @@ def captcha_solve(page: Page, password: str) -> None:
         logging.info("no captcha found. continuing...")
 
 
-def save_result(page: Page) -> None:
-    result_html: str = page.content()
-    with open("result.html", mode="w", encoding="utf-8") as f:
-        f.write(result_html)
+def file_save_decorator(func):
+    @wraps(func)
+    def wrapper(
+        page: Page, out_path: str | PathLike = f"out_{now_str()}", **kwargs
+    ) -> None:
+        with Path(out_path).open(**kwargs) as f:
+            result: str | bytes = func(page, out_path)
+            f.write(result)
+
+    return wrapper
 
 
-def buy_reload(page: Page, password: str) -> None:
+@file_save_decorator
+def save_html(page: Page, mode: str = "w", encoding: str = "utf-8") -> str:
+    return page.content()
+
+
+@file_save_decorator
+def save_screenshot(page: Page, mode="wb") -> bytes:
+    return page.screenshot()
+
+
+def buy_reload(
+    page: Page,
+    password: str,
+) -> None:
     captcha_solve(page, password)
     # check email verification
     try:
         logging.info("Checking email verification needed...")
-        page.wait_for_selector("#channelDetailsWithImprovedLayout", timeout=10000)
+        page.wait_for_selector(
+            "#channelDetailsWithImprovedLayout",
+        )
         logging.info("Email verification needed.")
-        logging.info("Press enter after verified your email.")
+        logging.info("Press enter verification code in your email.")
         input("ENTER TO CONTINUE: ")
         logging.info("continuing...")
     except TimeoutError:
@@ -121,7 +165,9 @@ def buy_reload(page: Page, password: str) -> None:
     # disable amazon currency converter
     try:
         logging.info("Trying to disable amazon currency converter...")
-        disable_radio: ElementHandle = page.wait_for_selector("#marketplaceRadio", timeout=10000)
+        disable_radio: ElementHandle = page.wait_for_selector(
+            "#marketplaceRadio",
+        )
         disable_radio.click()
         logging.info("clicked disable. waiting for order button...")
     except TimeoutError as e:
@@ -129,42 +175,42 @@ def buy_reload(page: Page, password: str) -> None:
 
     try:
         logging.info("waiting for order button...")
-        page.wait_for_selector('''input[type="submit"][name="placeYourOrder1"]''')
+        page.wait_for_selector(
+            """input[type="submit"][name="placeYourOrder1"]"""
+        )
         logging.info("found order button.")
     except TimeoutError:
         logging.info("no order button found. screenshotting...")
-        screenshot_buffer: bytes = page.screenshot()
-        # save screenshot
-        with open("screenshot.png", "wb") as f:
-            f.write(screenshot_buffer)
+        save_screenshot(page, out_path=f"error_{now_str()}.png")
         logging.info("extracting html...")
-        html: str = page.content()
         # save html
-        with open("html.html", mode="w", encoding="utf-8") as f:
-            f.write(html)
-    page.click('''input[type="submit"][name="placeYourOrder1"]''')
-    logging.info("clicked order button. waiting for order confirmation button ...")
+        save_html(page, out_path=f"error_{now_str()}.html")
+
+    # block below line if you want to not buy
+    page.click("""input[type="submit"][name="placeYourOrder1"]""")
+    logging.info(
+        "clicked order button. waiting for order confirmation button ..."
+    )
     page.wait_for_selector("#widget-purchaseConfirmationDetails")
-    logging.info("job finished. wait for 5 seconds to check result...")
-    page.wait_for_timeout(5000)
-    logging.info("waited for 10 seconds. quit...")
-    save_result(page)
+    logging.info("job finished. quit...")
+    save_html(page, out_path=f"result_{now_str()}.html")
     page.close()
 
 
-def process_reload_all(browser: Browser, email: str, password: str):
-    page: Page = init_page(browser)
-    price: str = str(get_5999_won_concurrency(page))
+def process_reload_all(
+    browser: Browser,
+    email: str,
+    password: str,
+    is_safe: bool = True,
+    default_timeout: int = GLOBAL_TIMEOUT,
+) -> None:
+    page: Page = init_page(browser=browser, default_timeout=default_timeout)
+    price: str = str(get_5999_won_concurrency(page=page, is_safe=is_safe))
     goto_amazon(page)
     type_price_and_submit(page, price)
     login(page, email, password)
     buy_reload(page, password)
     browser.close()
-
-
-class Config(TypedDict):
-    email: str
-    password: str
 
 
 def load_yaml_config(file_path: Path | str) -> Mapping[str, Any]:
@@ -175,10 +221,34 @@ def load_yaml_config(file_path: Path | str) -> Mapping[str, Any]:
         return yaml.safe_load(f)
 
 
+def parse_args():
+    parser: ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--safe",
+        default=True,
+        action=argparse.BooleanOptionalAction,
+    )
+    parser.add_argument(
+        "--timeout", default=5, type=int, help="timeout in seconds"
+    )
+    return parser.parse_args()
+
+
+class Config(TypedDict):
+    email: str
+    password: str
+
+
 if __name__ == "__main__":
     init_logger()
-    play_wright: Playwright = sync_playwright().start()
-    bs: Browser = init_browser(play_wright)
+    args: argparse.Namespace = parse_args()
+    pw_core: Playwright = sync_playwright().start()
+    playwright_browser: Browser = init_browser(pw_core)
     config: Config = Config(**load_yaml_config("config.yml"))
 
-    process_reload_all(bs, **config)
+    process_reload_all(
+        playwright_browser,
+        is_safe=args.safe,
+        default_timeout=GLOBAL_TIMEOUT,
+        **config,
+    )
